e4edb48a71b839b4a8a7d42c865a646d75c5c768
John Bellardo <bellardo@calpoly.edu>
2016-06-07 00:47:44 -0700

    Initial skel files

---
+++ cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -e etc/inittab.append ] ; then
+   cat etc/inittab.append >> etc/inittab
+   rm -f etc/inittab.append
+fi
+
+if [ -e usr/local/etc/inittab.append ] ; then
+   cat usr/local/etc/inittab.append >> usr/local/etc/inittab
+   rm -f usr/local/etc/inittab.append
+fi
+
+for dir in log lock run spool cache; do
+   rm -f var/$dir
+   mkdir -p var/$dir
+done
+
+rm -rf .stamp* dev-tag.sh .git
+
+chmod 755 .
+chmod 755 /root
+
+echo Built: `date` >> etc/issue
+echo >> etc/issue
+cp etc/issue usr/local/etc/issue
